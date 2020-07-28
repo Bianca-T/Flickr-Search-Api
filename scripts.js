b2c768010b899707c9d38e9ecd354b53d0c05d8c
Bianca Thompson <biacthompson@gmail.com>
2020-07-28 14:18:57 -0400

    Adding responsive Css design

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-// var url =
-// 	"https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e66ca1aefa9dee7abb5fcbcd4cc1189c&text=flowers&content_type=1&media=photos&per_page=25&page=1&format=json&nojsoncallback=1";
-
 const app = document.getElementById("root");
 
 const container = document.createElement("div");
@@ -26,6 +23,9 @@ function getImages(event) {
 			var images = image.photos.photo;
 			console.log(images);
 			for (var i = 0; i < image.photos.photo.length; i++) {
+				if (i === 26) {
+					break;
+				}
 				var currentPhotoUrl =
 					"https://farm" +
 					images[i]["farm"] +
